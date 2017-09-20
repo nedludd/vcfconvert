f31a473a9d9b8828fbbe0657235ff44b656bdbea
Tim Hermans <tim_hermans@cable.comcast.com>
2017-09-20 14:27:48 -0400

    Removed everything but CSV conversion.  Removed web page stuff.

--- utils.php
+++ utils.php
@@ -8,27 +8,27 @@
  */
 function parse_bytes($str)
 {
-	if (is_numeric($str))
-		return intval($str);
-		
-	if (preg_match('/([0-9]+)([a-z])/i', $str, $regs))
-	{
-		$bytes = floatval($regs[1]);
-		switch (strtolower($regs[2]))
-		{
-			case 'g':
-				$bytes *= 1073741824;
-				break;
-			case 'm':
-				$bytes *= 1048576;
-				break;
-			case 'k':
-				$bytes *= 1024;
-				break;
-		}
-	}
+    if (is_numeric($str))
+        return intval($str);
 
-	return intval($bytes);
+    if (preg_match('/([0-9]+)([a-z])/i', $str, $regs))
+    {
+        $bytes = floatval($regs[1]);
+        switch (strtolower($regs[2]))
+        {
+            case 'g':
+                $bytes *= 1073741824;
+                break;
+            case 'm':
+                $bytes *= 1048576;
+                break;
+            case 'k':
+                $bytes *= 1024;
+                break;
+        }
+    }
+
+    return intval($bytes);
 }
 
 /**
@@ -39,22 +39,22 @@ function parse_bytes($str)
  */
 function show_bytes($bytes)
 {
-	if ($bytes > 1073741824)
-	{
-		$gb = $bytes/1073741824;
-		$str = sprintf($gb >= 10 || $gb-intval($gb) == 0 ? "%d GB" : "%.1f GB", $gb);
-	}
-	else if ($bytes > 1048576)
-	{
-		$mb = $bytes/1048576;
-		$str = sprintf($mb >= 10 || $mb-intval($mb) == 0 ? "%d MB" : "%.1f MB", $mb);
-	}
-	else if ($bytes > 1024)
-		$str = sprintf("%d KB", round($bytes/1024));
-	else
-		$str = sprintf('%d B', $bytes);
+    if ($bytes > 1073741824)
+    {
+        $gb = $bytes/1073741824;
+        $str = sprintf($gb >= 10 || $gb-intval($gb) == 0 ? "%d GB" : "%.1f GB", $gb);
+    }
+    else if ($bytes > 1048576)
+    {
+        $mb = $bytes/1048576;
+        $str = sprintf($mb >= 10 || $mb-intval($mb) == 0 ? "%d MB" : "%.1f MB", $mb);
+    }
+    else if ($bytes > 1024)
+        $str = sprintf("%d KB", round($bytes/1024));
+    else
+        $str = sprintf('%d B', $bytes);
 
-	return $str;
+    return $str;
 }
 
 /**
@@ -63,7 +63,7 @@ function show_bytes($bytes)
  */
 function asciiwords($str)
 {
-	return preg_replace(array('/\s+/', '/[^a-z0-9\_\-\.]/i'), array('_',''), $str);
+    return preg_replace(array('/\s+/', '/[^a-z0-9\_\-\.]/i'), array('_',''), $str);
 }
 
 
